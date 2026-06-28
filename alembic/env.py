305fd5503cc38e504f0cec1c938a50f49ba201b9
Yashwanth Mettu <yashwanthme2@gmail.com>
2026-06-28 10:00:19 +0530

    fix: Ensure alembic async engine gets the correct asyncpg URL dynamically

--- alembic/env.py
+++ alembic/env.py
@@ -47,8 +47,10 @@ async def run_async_migrations() -> None:
     """In this scenario we need to create an Engine
     and associate a connection with the context.
     """
+    configuration = config.get_section(config.config_ini_section, {})
+    configuration["sqlalchemy.url"] = settings.get_database_url
     connectable = async_engine_from_config(
-        config.get_section(config.config_ini_section, {}),
+        configuration,
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
     )
